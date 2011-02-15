23b6d0ff0fbfc031c364cea4b8fbfa2d50e18e27
Justin Holmes <justin_h@hotmail.co.uk>
2011-02-15 13:58:33 +0000

    third commit

--- src/TestCase.scala
+++ src/TestCase.scala
@@ -1,26 +1,29 @@
-package com.example.tests
-
 import com.thoughtworks.selenium._
 import java.util.regex.Pattern
 
 import junit.framework._
 import junit.framework.TestSuite
 
- class SeleniumTest extends SeleneseTestCase {
-	override def setUp() = {
-		setUp("http://www.google.co.uk", "*firefox")
-	}
-	def testJ()= {
-		selenium.open("/")
-		selenium.click("search")
-		//selenium.click("link=Jstin Holmes");
-	}
+class SeleniumTest extends SeleneseTestCase {
+  override def setUp() = {
+    setUp("http://www.google.co.uk", "*chrome")
+  }
 
-  def suite()={
-     var clazz = this.getClass
-		 new TestSuite(clazz);
- }
-	object main extends Application {
-		junit.textui.TestRunner.run(suite())
-	}
+  def testJ() = {
+
+    selenium.open("/")
+    selenium.click("search")
+  }
+
+  def suite() = {
+    var clazz = this.getClass
+    new TestSuite(clazz);
+  }
+
+  def runSTest() = junit.textui.TestRunner.run(suite())
+}
+
+object Main extends Application {
+  val s = new SeleniumTest
+  s.runSTest()
 }
