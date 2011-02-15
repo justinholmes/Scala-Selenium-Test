e7de0b529ed93376ffe91e5fad1bbecc8ac1e82b
Justin Holmes <justin_h@hotmail.co.uk>
2011-02-15 12:18:47 +0000

    first commit

---
+++ src/TestCase.scala
@@ -0,0 +1,26 @@
+package com.example.tests;
+
+import com.thoughtworks.selenium._
+import java.util.regex.Pattern
+
+import junit.framework._
+import junit.framework.TestSuite
+
+ class SeleniumTest extends SeleneseTestCase {
+	override def setUp() = {
+		setUp("http://www.google.co.uk", "*firefox")
+	}
+	def testJ()= {
+		selenium.open("/")
+		selenium.click("search")
+		//selenium.click("link=Jstin Holmes");
+	}
+
+  def suite()={
+     var clazz = this.getClass
+		 new TestSuite(clazz);
+ }
+	object main extends Application {
+		junit.textui.TestRunner.run(suite())
+	}
+}
